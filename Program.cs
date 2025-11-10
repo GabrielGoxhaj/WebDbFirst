bb6caecff5004203652e98360352d5dcac748b95
Gabriel <gabrielgoxhaj2013@hotmail.it>
2025-11-10 14:03:41 +0100

    Updated to 10th November (Morning)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.IdentityModel.Tokens;
 using System.Text.Json.Serialization;
 using WebDbFirst.Models;
 
@@ -26,6 +28,42 @@ namespace WebDbFirst
 
             builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLT2019") ?? throw new InvalidOperationException("Connection string 'AdventureWorksLT2019' not found.")));
+
+            builder.Services.AddCors(
+                opts =>
+                {
+                    opts.AddPolicy("CorsPolicia",
+                    build => build
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .SetIsOriginAllowed((hosts) => true));
+                });
+
+            // JWT Authentication
+            //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im9ybG9mZiIsIm5iZiI6MTcxMzc4NzQxMCwiZXhwIjoxNzEzNzg3NDcwL
+            //    CJpYXQiOjE3MTM3ODc0MTAsImlzcyI6IlVubyIsImF1ZCI6IkR1ZSJ9._jCXEWeoIQpjZPexxhAUCvv6myYTttuKMp3z0dYnrhs
+
+            JwtSettings jwtSettings = new();
+            //builder.Configuration.Bind(nameof(JwtSettings), jwtSettings);
+            jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>()!;
+            builder.Services.AddSingleton(jwtSettings);
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    var key = System.Text.Encoding.ASCII.GetBytes(jwtSettings.SecretKey!);
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -35,6 +73,7 @@ namespace WebDbFirst
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("CorsPolicia");
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
